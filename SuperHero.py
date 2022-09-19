e7d4afcd03d4c2efe726f88135f8dc78311a2992
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2022-09-19 11:46:00 +0200

    add bidirectional link between Team and SuperHero

--- SuperHero.py
+++ SuperHero.py
@@ -7,11 +7,13 @@ from Power import Power
 class SuperHero(AgencyElement):
     __birthDate: date
     __powers: [Power]
+    __teams: []
 
     def __init__(self, name: str, birthDate: date):
         AgencyElement.__init__(self, name)
         self.__birthDate = datetime.strptime(birthDate, "%Y-%m-%d")
         self.__powers = []
+        self.__teams = []
 
     def getBirthDate(self) -> date:
         return self.__birthDate
@@ -27,9 +29,16 @@ class SuperHero(AgencyElement):
         for power in self.__powers:
             print(power.getName() + " ", end="")
         print(")")
+        for team in self.__teams:
+            print("\t-", team.getName())
 
     def getPowers(self) -> str :
         powers = ""
         for power in self.__powers:
             powers += power.getName() + ", "
         return powers
+
+    def addTeam(self, team):
+        if team not in self.__teams:
+            self.__teams.append(team)
+            team.addSuperHero(self)
